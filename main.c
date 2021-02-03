b523f888a44646219ae74d206dd5edb662196a90
Jean-Yves Célis <42608645+jyce3d@users.noreply.github.com>
2021-02-03 13:03:24 +0100

    high jmp

--- main.c
+++ main.c
@@ -16,9 +16,10 @@
 
 #define CEP_DIR_RIGHT 0
 #define CEP_DIR_LEFT 1
-#define CEP_JMP_RIGHT 2
-#define CEP_JMP_LEFT 4
 
+#define CEP_JMP_DIR 0
+#define CEP_JMP_UP 1
+#define CEP_JMP_LONG 1
 
 /* Initialization des composants */
 SDL_Window* pWindow = NULL;
@@ -39,7 +40,7 @@ KeyStatus a_ks[4];
 #define KS_SPACE 3
 
 #define KS_KEYDOWN 1
-#define KS_KEYUP 0;
+#define KS_KEYUP 2
 
 
 
@@ -82,6 +83,7 @@ SDL_Surface* level_sfc = NULL;
 SDL_Texture* pScene = NULL;
 
 SDL_Texture* pPlayer = NULL;
+SDL_Texture* pEnemy = NULL;
 
 /* Définition des mosaiques */
 SDL_Rect space = {32,0,8,8};
@@ -97,20 +99,69 @@ SDL_Rect spr_player_left[3] = { {2,16, 15, 16}, {17, 16, 15, 16}, {52,16, 12,16}
 SDL_Rect spr_player_jmpr = {16,0,15, 16};
 SDL_Rect spr_player_jmpl = {32,16,15, 16};
 
+// Paramètres
+int score = 0;
+int high_score =0;
+int vie = 3;
+
+// Enemy
+SDL_Rect spr_enemy[3]= { {0,0,16,16 }, {16,0,16,16}, {32,0,16,16} };
+SDL_Rect spr_enemy_dest = {0, 0, 16,16};
+int enemy_x = 16;
+int enemy_y = 84;
+int enemy_sens_x=0;
+
 
 SDL_Rect spr_player_dest = {0,0,16,16};
 int play_x = 16;
 int play_y = 136;
 
-unsigned int direction = CEP_DIR_RIGHT;
+void deplace_enemy() {
+    int pos_en_y = (enemy_y +16) / 8;
+    int pos_en_x = enemy_x / 8;
+   // printf("j=%d, i=%d, contenu=%d\n", pos_en_y, pos_en_x, ga_scene[pos_en_y][pos_en_x]);
+    if (ga_scene[pos_en_y][pos_en_x]==0) {
+     enemy_y++;
+     enemy_sens_x=0;
+    }
+    else {
+        if (enemy_sens_x ==0 ) {
+         if ( ((random()*10) % 10)>=1) enemy_sens_x=1;
+            else enemy_sens_x = -1;
+        }
+        if (enemy_sens_x ==-1) {
+            if ( (enemy_x--)<=0)
+             enemy_sens_x =1;
+            } else if (enemy_sens_x ==1) {
+                if ((enemy_x++)>=304) enemy_sens_x =-1;
+            }
+    }
+    spr_enemy_dest.x = enemy_x;
+    spr_enemy_dest.y = enemy_y;
+
+    SDL_RenderCopy(pRenderer, pEnemy, &spr_enemy[enemy_x %3], &spr_enemy_dest);
+
+}
+
+unsigned int direction = CEP_DIR_RIGHT; // bit 15=1 => jmp , 
+unsigned int jmp_direction = CEP_JMP_DIR;
+unsigned int jmp_type = 0;
+Uint32 lastup_down=0;
+
+int x_saut_index = 0;
+
 int costume =0;
 /* Définition des fonctions */
-int Trajectoire_Saut( int x) {
+int Trajectoire_Saut( int x_saut) {
     int y;
-    if (x<24)
-        y=-5.33f /4;
+    float delta = 5.33f;
+    printf("t_s:jmp_type=%d\n", jmp_type);
+    if (jmp_type==CEP_JMP_LONG)
+        delta = 2.5f *delta;
+    if (x_saut>24)
+        y=delta /2;
     else    
-        y=5.33f /4 ;
+        y=-delta /2 ;
     return y;
 }
 
@@ -123,7 +174,7 @@ int Create() {
     pWindow = SDL_CreateWindow("CEP - Certificat D'étude Primate", 
                                 SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                                 CEP_WND_WIDTH, CEP_WND_HEIGHT,SDL_WINDOW_SHOWN );
-    SDL_SetWindowFullscreen(pWindow, SDL_WINDOW_FULLSCREEN);
+   // SDL_SetWindowFullscreen(pWindow, SDL_WINDOW_FULLSCREEN);
                             
     if (pWindow) {
             if (!TTF_Init()) {
@@ -140,6 +191,10 @@ int Create() {
                         SDL_SetColorKey(player_sfc, SDL_TRUE, SDL_MapRGB(player_sfc->format, 0xFF, 0xFF, 0xFF));
                         pPlayer = SDL_CreateTextureFromSurface(pRenderer, player_sfc);
                         SDL_FreeSurface(player_sfc);
+                        SDL_Surface* enemy_sfc = IMG_Load("enemy.bmp");
+                        SDL_SetColorKey(enemy_sfc, SDL_TRUE, SDL_MapRGB(enemy_sfc->format, 0xFF, 0xFF, 0xFF));
+                        pEnemy = SDL_CreateTextureFromSurface(pRenderer, enemy_sfc);
+                        SDL_FreeSurface(enemy_sfc);
                         return 1;
                     }
                 }
@@ -155,6 +210,7 @@ int Create() {
 }
 
 int Destroy() {
+    SDL_DestroyTexture(pEnemy);
     SDL_DestroyTexture(pPlayer);
     SDL_DestroyTexture(pScene);
     SDL_FreeSurface(level_sfc);
@@ -241,11 +297,28 @@ void Init_Scene() {
     SDL_FreeSurface(scene_sfc);
 
 }
+// Return true if still jumping
+int jmp_right_criteria() {
+    int ga_r = (play_y+16) / 8;
+    int ga_c = (play_x+16) / 8 ;
+    return (ga_scene[ga_r][ga_c] ==0);
+}
+int jmp_left_criteria() {
+    int ga_r = (play_y+16) / 8;
+    int ga_c = (play_x) / 8 ;
+    return (ga_scene[ga_r][ga_c] ==0);
+
+}
 void Update_Game() {
+ 
     Uint32 cur_time = SDL_GetTicks() - last_time;
     if (cur_time<=30) {
         SDL_Delay(30 - cur_time);
     } else {
+        // Affichage des paramètres:
+     
+
+
         last_time = SDL_GetTicks();
         if (sfc_loaded==0) {
             Init_Scene();
@@ -257,13 +330,10 @@ void Update_Game() {
         SDL_RenderCopy(pRenderer, pScene, &rct_scene, &rct_scene );
 
 
-
-        // Process Enemy
-
         // Collision
 
         // Process Display
-       // printf("%d\n", direction);
+ 
         if ((direction & 32768)==0) {
             if (a_ks[KS_RIGHT].status == KS_KEYDOWN) {
                 if ( (play_x+=2)>=304 )
@@ -284,7 +354,15 @@ void Update_Game() {
                     x_saut = 48;
                 else x_saut=0;
                 direction |= 32768;
-            }
+                if ((a_ks[KS_RIGHT].status == KS_KEYUP) && (a_ks[KS_LEFT].status == KS_KEYUP))
+                    jmp_direction = CEP_JMP_UP;
+                else {
+                    jmp_direction = CEP_JMP_DIR;
+                }
+                if (lastup_down == 0)
+                    lastup_down = SDL_GetTicks();
+            } 
+
             spr_player_dest.x = play_x;
             spr_player_dest.y = play_y;
             if (direction == CEP_DIR_RIGHT) 
@@ -293,23 +371,54 @@ void Update_Game() {
                 SDL_RenderCopy(pRenderer, pPlayer, &spr_player_left[costume], &spr_player_dest);
         } else {
             // Process JMP
+          //  int pos_pl_y = (play_y +16) / 8;
+           // int pos_pl_x = play_x / 8;
+  //          int pos_pl_x_r = (play_x + 16) / 8;
+   //         int pos_pl_y_up =(play_y) /8;
+            // 1. Detect long jmp
+            if (a_ks[KS_UP].status == KS_KEYUP) {
+                if (lastup_down>0) {
+                   int diff = (SDL_GetTicks() - lastup_down);
+                    if ( diff >= 100) {
+                        if (direction >=32767)  jmp_type = CEP_JMP_LONG; else jmp_type =0;
+                    }
+                    printf("diff = %d\nstatus  =%d\n lastupd=%d\n jmptype=%d\n", diff, a_ks[KS_UP].status, lastup_down, jmp_type) ;
+
+                    lastup_down = 0;
+                }
+            }
+            if (jmp_direction == CEP_JMP_UP)
+                x_saut_index = 0; else x_saut_index=2;
             if ((direction & 32767) == CEP_DIR_LEFT) {
-                if ((play_x-=2)<=0)
+                if ((play_x-=x_saut_index)<=0)
                     play_x = 0;
                 x_saut -=2;
-        //        printf("%d %d\n", x_saut, play_x);
                 play_y = play_y - Trajectoire_Saut(x_saut);
-                if (x_saut<=0) {
+                if ((x_saut<24) && !jmp_left_criteria()) {
+                    play_y -= play_y %8;
                     direction &=32767;
+                    jmp_type=0;
                 }
             } else if ((direction & 32767) == CEP_DIR_RIGHT ) {
-                if ((play_x+=2)>=304)
+                if ((play_x+=x_saut_index)>=304)
                     play_x= 304;
                 x_saut +=2;
                 play_y = play_y + Trajectoire_Saut(x_saut);
-                if (x_saut >=44) {
+ 
+                // DEBUG
+               if (jmp_type ==1)
+                    printf("1)play y =%d, x_saut=%d\n", play_y, x_saut);
+
+//                if (ga_scene[pos_pl_y][pos_pl_x_r]!=0)
+                if ((x_saut>24) && !jmp_right_criteria()  ) {
+                    play_y -= play_y %8;
                     direction &=32767;
+                    jmp_type=0;
                 }
+                                                   // DEBUG:
+                if (jmp_type ==1)
+                    printf("2)play y =%d", play_y);
+
             }
             spr_player_dest.x = play_x;
             spr_player_dest.y = play_y;
@@ -319,52 +428,20 @@ void Update_Game() {
                 SDL_RenderCopy(pRenderer, pPlayer, &spr_player_jmpl, &spr_player_dest);
 
         }
+        // Process Enemy
+
+        deplace_enemy();
     }
+    SDL_SetRenderDrawColor(pRenderer, 0,0,0, 255);
+    SDL_Color color = {255,255,255};
+    char s_params[255];
+    sprintf(s_params, "Score=%d        HIGH Score= %d", score, high_score);
+    Display_Text(s_params, CEP_TXT_LEFT , &color, 0,164);
+    sprintf(s_params, "Vie(s)=%d", vie);
+    Display_Text(s_params, CEP_TXT_LEFT , &color, 0,180);
+
     SDL_RenderPresent(pRenderer);
 }
-
-/*void Process_Game(SDL_Event *pEvent) {
-    SDL_KeyboardEvent *pKey;
-    
-    
-    switch (pEvent->type) {
-        case SDL_KEYUP:
-            if ((direction & 32768) ==0) 
-                costume =0;
-        break;
-            case SDL_KEYDOWN:
-                pKey = &(pEvent->key);
-                if (pKey->keysym.sym==SDLK_RIGHT) {
-                    if ((direction & 32768) ==0) {
-                       if ( (play_x+=4)>=304 )
-                            play_x = 304;
-                        direction = CEP_DIR_RIGHT;
-                        costume ++;
-                        costume = costume % 3;
-                    }
-                } else if (pKey->keysym.sym == SDLK_LEFT) {
-                    if ((direction & 32768) ==0)
-                    {
-                       if ( (play_x-=4)<=0)
-                            play_x = 0;
-                        direction = CEP_DIR_LEFT;
-                        costume ++;
-                        costume = costume % 3;
-                    }
-                } else if (pKey->keysym.sym == SDLK_SPACE) {
-                    if ((direction & 32768) ==0) {
-                        if (direction == CEP_DIR_LEFT)
-                            x_saut = 48;
-                        else x_saut=0;
-                        direction |= 32768;
-                    }
-                    
-                }
-            break;
-
-        }
-  
-}*/
 
 void update_status() {
             switch(status) {
@@ -385,7 +462,6 @@ void main() {
             
          
             update_status();
-            //if (status == CEP_STATUS_GAME)
               //  Process_Game();
             while (SDL_PollEvent(&event) != 0) {
                 switch (event.type) {
@@ -418,17 +494,8 @@ void main() {
                         }
                     break;
                     default:
-   /*                     switch(status) {
-                            case CEP_STATUS_INTRO:
-                                Process_Intro(&event);
-                            break;
-                            case CEP_STATUS_GAME:
-                                Process_Game(&event);
-                            break;
-                        }*/
                     break;
                 }
-               // update_status();
             }
         }
 
